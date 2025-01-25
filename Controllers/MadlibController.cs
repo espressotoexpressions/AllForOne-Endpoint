8af0796f2c2ad0e87f5e0039b7b51f50c17b4fe6
espressotoexpressions <keannevillar@gmail.com>
2025-01-25 11:21:44 -0800

    update madlibs

--- Controllers/MadlibController.cs
+++ Controllers/MadlibController.cs
@@ -21,7 +21,7 @@ namespace AllForOne_Endpoint.Controllers
         }
 
         [HttpGet]
-        [Route("Madlib/{noun1},{adjective2},{number3}, {noun4},{noun5},{adjective6},{noun7},{number8},{number9},{adjective10},{adverb11},{noun12}")]
+        [Route("Madlib/{noun1}/{adjective2}/{number3}/{noun4}/{noun5}/{adjective6}/{noun7}/{number8}/{number9}/{adjective10}/{adverb11}/{noun12}")]
 
         public string GenerateStory(string noun1, string adjective2, string number3, string noun4, string noun5, string adjective6, string noun7, string number8, string number9, string adjective10, string adverb11, string noun12)
         {
